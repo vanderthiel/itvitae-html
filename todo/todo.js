4fbaeda10ebae5317f63a078f636959e3355d8a9
Thiel, Edwin van der <edwin.van.der.thiel@sogeti.com>
2019-12-17 14:05:19 +0100

    werkend met localstorage

--- todo/todo.js
+++ todo/todo.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+  let opslag = [];
+
+  let local = localStorage.getItem("opslag");
+  if(local && local.length){
+    opslag = JSON.parse(local);
+  }
+
   $("#save").click(function () {
-    $("#result").append('<li>' + $("#todo").val() + '</li>');
+    let item = $("#todo").val();
+    $("#result").append('<li>' + item + '</li>');
+
+    opslag.push(item);
+    localStorage.setItem("opslag", JSON.stringify(opslag));
   });
 });
