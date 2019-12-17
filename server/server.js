535cbe0ba19e1f7da52b531e588049c61b28c4b8
Thiel, Edwin van der <edwin.van.der.thiel@sogeti.com>
2019-12-17 14:28:29 +0100

    update cors settings, standaard data

--- server/server.js
+++ server/server.js
@@ -1,24 +1,34 @@
 // Get dependencies
 const express = require('express');
+var cors = require('cors');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const rp = require('request-promise-native');
 
 const app = express();
+app.use(cors());
 
-let mem = [{test: "bla" }];
+let mem = ["eerste \" item", "tweede item", "derde item"];
 
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
 
 app.get('/', (req, res) => {
 	res.status(200).send("This is the default endpoint, it is not the endpoint you're looking for");
 });
 
 app.get('/content', (req, res) => {
-	res.status(200).send(mem[0]);
+	res.status(200).send(mem);
+});
+
+app.post('/content', (req, res) => {
+	mem.push(req.body);
+	res.status(201).send(req.body);
 });
 
 /**
