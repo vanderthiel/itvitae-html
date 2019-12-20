2448460776c7900e8e6dee7fb0d2f077ea9255a5
Thiel, Edwin van der <edwin.van.der.thiel@sogeti.com>
2019-12-20 13:36:17 +0100

    comments

--- webworker/webworker.js
+++ webworker/webworker.js
@@ -1,10 +1,13 @@
 var w;
 
 function startWorker() {
+  // Check of Worker door de browser wordt ondersteund
   if (typeof (Worker) !== "undefined") {
     if (typeof (w) == "undefined") {
+      // maak een nieuw worker proces op een aparte thread
       w = new Worker("worker.js");
     }
+    // Koppel een event van de worker, afgegeven via postMessage
     w.onmessage = function (event) {
       document.getElementById("result").innerHTML = event.data;
     };
@@ -14,6 +17,7 @@ function startWorker() {
 }
 
 function stopWorker() {
+  // stop de worker. de aparte thread wordt afgesloten
   w.terminate();
   w = undefined;
 }
--- webworker/worker.js
+++ webworker/worker.js
@@ -1,6 +1,12 @@
+// worker start en draait zo lang hij actief is.
+// In dit geval door setTimeout
+
+// Let op: de DOM is niet beschikbaar
+
 var i = 0;
 
 function timedCount() {
+  // stuur een bericht terug naar het proces dat deze worker gestart heeft
   postMessage(i++);
   setTimeout(timedCount, 500);
 }
